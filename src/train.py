4a29ada541614139234fc5cab3d47a769466705e
Joseph Dobson <75431083+josephldobson@users.noreply.github.com>
2024-04-04 17:43:25 +0100

    cleaning

--- src/train.py
+++ src/train.py
@@ -56,9 +56,9 @@ def train_and_save(peft_method, model_name, batch_size, num_epochs):
 
 
 if __name__ == '__main__':
-    for PEFT_METHOD in ["LORA"]:
+    for PEFT_METHOD in ["ADALORA", "DORA", "IA3", "LORA", "P_TUNING", "PREFIX_TUNING", "PROMPT_TUNING"]:
         MODEL_NAME = "google/flan-t5-base"
         BATCH_SIZE = 64
-        NUM_EPOCHS = 15
+        NUM_EPOCHS = 10
 
         train_and_save(PEFT_METHOD, MODEL_NAME, BATCH_SIZE, NUM_EPOCHS)
