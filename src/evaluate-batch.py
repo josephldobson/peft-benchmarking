c3881cc9ca765b36e7388f548518fc63ecb51b67
Joseph Dobson <75431083+josephldobson@users.noreply.github.com>
2024-04-08 11:18:34 +0100

    eval

--- src/evaluate-batch.py
+++ src/evaluate-batch.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     results_dir = 'results'
     os.makedirs(results_dir, exist_ok=True)
 
-    for PEFT_METHOD in ["PROMPT_TUNING", "LORA"]:
+    for PEFT_METHOD in ["P_TUNING", "PREFIX_TUNING"]:
         print(PEFT_METHOD)
         test_acc, subject_acc = eval_mmlu(f'models/google/flan-t5-base_{PEFT_METHOD}_1')
 
@@ -155,15 +155,15 @@ if __name__ == '__main__':
         with open(subject_accs_file_path, 'wb') as handle:
             pkl.dump(subject_acc, handle)
 
-    PEFT_METHOD = "google/flan-t5-base"
-    test_acc, subject_acc = eval_mmlu(f'models/google/flan-t5-base_{PEFT_METHOD}_1', PEFT=False)
+    # PEFT_METHOD = "google/flan-t5-base"
+    # test_acc, subject_acc = eval_mmlu(f'models/google/flan-t5-base_{PEFT_METHOD}_1', PEFT=False)
 
-    # Forming the file paths
-    acc_file_path = os.path.join(results_dir, f'flan-t5-base_{PEFT_METHOD}_1_MMLU-acc.pickle')
-    subject_accs_file_path = os.path.join(results_dir, f'flan-t5-base_{PEFT_METHOD}_1_subject-accs.pickle')
+    # # Forming the file paths
+    # acc_file_path = os.path.join(results_dir, f'flan-t5-base_{PEFT_METHOD}_1_MMLU-acc.pickle')
+    # subject_accs_file_path = os.path.join(results_dir, f'flan-t5-base_{PEFT_METHOD}_1_subject-accs.pickle')
 
-    with open(acc_file_path, 'wb') as handle:
-        pkl.dump(test_acc, handle)
+    # with open(acc_file_path, 'wb') as handle:
+    #     pkl.dump(test_acc, handle)
 
-    with open(subject_accs_file_path, 'wb') as handle:
-        pkl.dump(subject_acc, handle)
+    # with open(subject_accs_file_path, 'wb') as handle:
+    #     pkl.dump(subject_acc, handle)
