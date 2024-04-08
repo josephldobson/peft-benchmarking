550998a3dd51090c255e119589bd26dc1855156d
Joseph Dobson <75431083+josephldobson@users.noreply.github.com>
2024-04-08 08:54:07 +0100

    eval

--- src/evaluate-batch.py
+++ src/evaluate-batch.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     results_dir = 'results'
     os.makedirs(results_dir, exist_ok=True)
 
-    for PEFT_METHOD in ["ADALORA", "DORA", "IA3", "LORA", "P_TUNING", "PREFIX_TUNING", "PROMPT_TUNING"]:
+    for PEFT_METHOD in ["LORA", "P_TUNING", "PREFIX_TUNING", "PROMPT_TUNING"]:
         test_acc, subject_acc = eval_mmlu(f'models/google/flan-t5-base_{PEFT_METHOD}_1')
 
         # Forming the file paths
